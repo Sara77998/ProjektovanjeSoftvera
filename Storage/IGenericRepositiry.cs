f2286e904c143917564d1402a3217b74d40a6298
Sara Canovic <saracanovic@gmail.com>
2021-09-08 10:36:30 +0200

    promena 2

---
+++ Storage/IGenericRepositiry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+namespace Storage
+{
+    public interface IGenericRepositiry
+    {
+        void Save(IEntity entity);
+        List<IEntity> GetAll(IEntity e);
+        int GetNewId(IEntity e);
+
+        void OpenConnection();
+        void CloseConnection();
+        void BeginTransaction();
+        void Commit();
+        void Rollback();
+    }
+}
